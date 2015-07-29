848b67d5c93392ffb4727457e42d42aef81c5cce
Nathan Clisby <clisby@gmail.com>
2015-07-29 14:56:03 +1000

    Changed catch-22 quote

--- snippets/snippet0014.js
+++ snippets/snippet0014.js
@@ -1,1 +1,1 @@
-document.write('<div class="quote">Everyone agreed that Clevinger was certain to go far in the academic world. In short, Clevinger was one of those people with lots of intelligence and no brains, and everyone knew it except those who soon found it out. In short, he was a dope.</div> Joseph Heller in <em>Catch-22</em>.');
+document.write('<div class="quote">Yossarian ... had decided to live forever or die in the attempt,</div> Joseph Heller in <em>Catch-22</em>.');
