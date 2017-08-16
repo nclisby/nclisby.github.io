997c8c24d43cf40ab6ac026a9663b0fb256efd39
Nathan Clisby <clisby@gmail.com>
2017-08-17 00:05:59 +1000

    Improved small alpha behaviour of long-range walks.

--- projects/lrw/longrangerw.js
+++ projects/lrw/longrangerw.js
@@ -5,6 +5,7 @@
 var path = [];
 var n = 100;
 var a = 1.0;
+var minWidth = true;
 
 
 //function generate_hamiltonian_path(n,q)
@@ -45,16 +46,47 @@ function draw_path()
     var ymin = y;
     var ymax = y;
     var r;
-    var circle = document.createElementNS(svgns, 'circle');
-    circle.setAttributeNS(null, 'cx', x);
-    circle.setAttributeNS(null, 'cy', y);
-    circle.setAttributeNS(null, 'r', 0.5);
-    circle.setAttributeNS(null, 'style', 'fill: black; ' );
-    svg.appendChild(circle);
     //var dist2;
     for (i=0; i<n; i++)
     {
         r = Math.pow(1.0-path[i][1],-1.0/a)
+        //r = Math.exp(-Math.log(1.0-path[i][1])/a)
+        dx = Math.cos(path[i][0])*r
+        dy = Math.sin(path[i][0])*r
+        x = x + dx;
+        y = y + dy;
+        if (x < xmin) xmin = x;
+        if (x > xmax) xmax = x;
+        if (y < ymin) ymin = y;
+        if (y > ymax) ymax = y;
+    }
+    var aa = xmin-1
+    var bb = ymin-1
+    var cc = xmax+1-aa
+    var dd = ymax+1-bb
+    var strokeWidth;
+    if (minWidth) {
+        strokeWidth = 0.004*0.5*(cc+dd);
+        if (strokeWidth < 0.35)
+        {
+            strokeWidth = 0.35;
+        }
+    }
+    x = 0.0;
+    y = 0.0;
+    var circle = document.createElementNS(svgns, 'circle');
+    circle.setAttributeNS(null, 'cx', x);
+    circle.setAttributeNS(null, 'cy', y);
+    circle.setAttributeNS(null, 'r', 0.4);
+    circle.setAttributeNS(null, 'style', 'fill: black; ' );
+    svg.appendChild(circle);
+    //poorly behaved as alpha approaches zero, likely due to an
+    //underflow error of some sort - an issue with very short line
+    //segments
+    for (i=0; i<n; i++)
+    {
+        r = Math.pow(1.0-path[i][1],-1.0/a)
+        //r = Math.exp(-Math.log(1.0-path[i][1])/a)
         dx = Math.cos(path[i][0])*r
         dy = Math.sin(path[i][0])*r
         var line = document.createElementNS(svgns, 'line');
@@ -63,7 +95,14 @@ function draw_path()
         line.setAttributeNS(null, 'x2', x+dx);
         line.setAttributeNS(null, 'y2', y+dy);
         //line.setAttributeNS(null, 'style', 'stroke:rgb(0,0,0);stroke-width:0.5; ' );
-        line.setAttributeNS(null, 'style', 'stroke:rgb(0,0,0);stroke-width:0.35; ' );
+        if (minWidth)
+        {
+            line.setAttributeNS(null, 'style', 'stroke:rgb(0,0,0);stroke-width:'+strokeWidth+'; ' );
+        }
+        else
+        {
+            line.setAttributeNS(null, 'style', 'stroke:rgb(0,0,0);stroke-width:0.35; ' );
+        }
         svg.appendChild(line);
         x = x + dx;
         y = y + dy;
@@ -74,19 +113,42 @@ function draw_path()
         circle.setAttributeNS(null, 'r', 0.4);
         circle.setAttributeNS(null, 'style', 'fill: black; ' );
         svg.appendChild(circle);
-        if (x < xmin) xmin = x;
-        if (x > xmax) xmax = x;
-        if (y < ymin) ymin = y;
-        if (y > ymax) ymax = y;
     }
+    //for (i=0; i<n; i++)
+    //{
+        //r = Math.pow(1.0-path[i][1],-1.0/a)
+        //dx = Math.cos(path[i][0])*r
+        //dy = Math.sin(path[i][0])*r
+        //var line = document.createElementNS(svgns, 'line');
+        //line.setAttributeNS(null, 'x1', x);
+        //line.setAttributeNS(null, 'y1', y);
+        //line.setAttributeNS(null, 'x2', x+dx);
+        //line.setAttributeNS(null, 'y2', y+dy);
+        ////line.setAttributeNS(null, 'style', 'stroke:rgb(0,0,0);stroke-width:0.5; ' );
+        //line.setAttributeNS(null, 'style', 'stroke:rgb(0,0,0);stroke-width:0.35; ' );
+        //svg.appendChild(line);
+        //x = x + dx;
+        //y = y + dy;
+        //var circle = document.createElementNS(svgns, 'circle');
+        //circle.setAttributeNS(null, 'cx', x);
+        //circle.setAttributeNS(null, 'cy', y);
+        ////circle.setAttributeNS(null, 'r', 0.5);
+        //circle.setAttributeNS(null, 'r', 0.4);
+        //circle.setAttributeNS(null, 'style', 'fill: black; ' );
+        //svg.appendChild(circle);
+        //if (x < xmin) xmin = x;
+        //if (x > xmax) xmax = x;
+        //if (y < ymin) ymin = y;
+        //if (y > ymax) ymax = y;
+    //}
         //dist2 = dx*dx+dy*dy;
         //alert(dist2);
-    var a = xmin-1
-    var b = ymin-1
-    var c = xmax+1-a
-    var d = ymax+1-b
+    //var a = xmin-1
+    //var b = ymin-1
+    //var c = xmax+1-a
+    //var d = ymax+1-b
     //svg.setAttribute("viewBox", " "+(xmin-1.0)+" "+(ymin-1.0)+" "+(xmax+1.0)+" "+(ymax+1.0)+" "); 
-    svg.setAttribute("viewBox", " "+a+" "+b+" "+c+" "+d+" "); 
+    svg.setAttribute("viewBox", " "+aa+" "+bb+" "+cc+" "+dd+" "); 
     return;
  }
 
